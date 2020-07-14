557395fa8c3db42d47e63fe4afa6e4ac6ebe45a7
Maurycy Gaworowski <maurycy@seleco.pl>
2020-07-14 18:48:13 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+      var a = 1;
+      var b = 2;
+      var c = a + b;
+      const name = 'Maurycy';
+    //   alert(c);
+      console.log(`Nazywam się ${name}, i co z tego? Mam jeszcze ${c} fajki, ale nadal mam na imię ${name}`);
